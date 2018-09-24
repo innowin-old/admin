73833f35ebff830ca9f9f1cbf8e84fbd3477bfc9
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-09-24 17:02:56 +0330

    Close #11

--- pages/users/researches/form.vue
+++ pages/users/researches/form.vue
@@ -24,9 +24,21 @@
 
                         <v-text-field
                                 label=" نویسنده "
-                                v-model="model.author"
+                                v-model="author_text"
                                 :counter="100"
-                        ></v-text-field>
+                                @keyup.enter.native="appendAuthor"
+                        >
+                        </v-text-field>
+
+                        <div class="array-container">
+                            <div class="array-item" v-for="author_item in author">
+                                {{author_item}}
+                                <v-icon
+                                        @click="clearAuthor(author_item)"
+                                >clear</v-icon>
+                            </div>
+                        </div>
+                        <div style="clear: both"></div>
 
                         <v-text-field
                                 label=" انتشارات "
@@ -92,6 +104,7 @@
             id: null,
             isEditing: true,
             research_user_states: [],
+            author_text: '',
             title: '',
             titleRules: [
                 (v) => !!v || 'title is required',
@@ -138,6 +151,16 @@
                 if (!results) return null
                 if (!results[2]) return ''
                 return decodeURIComponent(results[2].replace(/\+/g, ' '))
+            },
+            appendAuthor () {
+                console.log(this.$data.author_text)
+                this.model.author.push(this.$data.author_text)
+                this.$data.author.push(this.$data.author_text)
+                this.$data.author_text = ''
+            },
+            clearAuthor (author_name) {
+                this.$data.author.splice(this.$data.author.indexOf(author_name), 1)
+                this.model.author.splice(this.model.author.indexOf(author_name), 1)
             }
         },
         computed: {
@@ -161,11 +184,27 @@
             if (this.model.research_user !== undefined) {
                 const user_object = this.$store.state.users.list.find(element => element._id === this.model.research_user)
                 this.model.research_user = user_object.username
+
+                for (let i = 0; i < this.model.author.length; i++) {
+                    this.$data.author.push(this.model.author[i])
+                }
             }
         }
     }
 </script>
 
 <style scoped>
+
+    .array-container {
+        padding-top: 10px;
+        padding-bottom: 20px;
+    }
+    .array-item {
+        float: left;
+        margin-left: 20px;
+        padding: 10px;
+        background: grey;
+        border-radius: 5px;
+    }
 
 </style>
